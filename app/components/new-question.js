4346ead64fde80861abb1c9897fa6a1a202aec4f
Shelby Clayton <shelby_clayton@hotmail.com>
2016-10-28 16:43:04 -0700

    add corrections to update form.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    saveQuestion() {
+    saveQuestion1() {
       var params = {
         userQuestion: this.get('userQuestion'),
-        author: this.get('author'),
+        author: this.get('questionAuthor'),
         notes: this.get('notes'),
         screenshot: this.get('screenshot'),
       };
-      this.sendAction('saveQuestion', params);
+      this.sendAction('saveQuestion2', params);
     }
   }
 });
--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
   actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
     update(question) {
       var params = {
-        userQuestion: this.get('updateUserQuestion'),
-        author: this.get('updateAuthor'),
-        notes: this.get('updateNotes'),
-        screenshot: this.get('updateScreenshot'),
+        userQuestion: this.get('userQuestion'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        screenshot: this.get('screenshot'),
       };
+      this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
     }
   }
